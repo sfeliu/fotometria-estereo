652cd2bd296f82f830d851b1a40900126f3665b7
Santiago Feliu <sfeliu@dc.uba.ar>
2017-08-24 16:42:16 -0300

    incluyo que pida el archivo en el cual se quiere buscar el punto mas brillante. También agrego los puntos mas brillantes de mate, en el txt.

--- ppmloader/main.cpp
+++ ppmloader/main.cpp
@@ -2,7 +2,9 @@
 #include<stdio.h>
 #include "ppmloader.h"
 #include<vector>
-
+#include <string>
+#include <sstream>
+// centro esfera, x = 244, y = 144
 using namespace std;
 using std::vector;
 typedef unsigned char uchar;
@@ -52,23 +54,72 @@ vector<unsigned int> get_max_pixel(std::string filename){
     read_image(filename, &data, &width, &height);
     vector<unsigned int> max (2,0);
     unsigned int max_value = get_pixel_average(data, max.at(0), max.at(1), height, width);
-
+    std::cout << width << std::endl;
+    std::cout << height << std::endl;
+    int count = 0;
     for (int h = 0; h < height; ++h){
         for(int w = 0; w < width; ++w){
             unsigned int temporal_value = get_pixel_average(data, h, w , height, width);
-            if (max_value < temporal_value){
+            if (temporal_value > 0){
+                count++;
+            }
+            if (max_value <= temporal_value){
                 max[0] = w;
                 max[1] = h;
                 max_value = temporal_value;
                 std::cout << "New max value: " << std::endl;
                 std::cout << max_value << std::endl;
+                std::stringstream ss;
+                ss << "Position of temporal maximum brightness: x = " << max.at(0) << " y = " << max.at(1) << std::endl;
+                std::cout << ss.str();
             }
         }
     }
+//    std::cout << count << std::endl;
+
     return max;
 
 };
 
+unsigned int get_radio(std::string filename){
+    uchar* data = NULL;
+    int width = 0, height = 0;
+    read_image(filename, &data, &width, &height);
+    vector<unsigned int> highest (2, height - 1);
+    vector<unsigned int> lowest (2,0);
+    vector<unsigned int> leftest (2, width - 1);
+    vector<unsigned int> rightest (2, 0);
+    std::stringstream ss;
+    for (int h = 0; h < height; ++h){
+        for(int w = 0; w < width; ++w){
+            unsigned int temporal_value = get_pixel_average(data, h, w , height, width);
+            if(temporal_value > 0){
+                if(h > lowest.at(1)){
+                    lowest[1] = h;
+                    lowest[0] = w;
+                }
+                if(h < highest.at(1)){
+                    highest[1] = h;
+                    highest[0] = w;
+                }
+                if(w < leftest.at(0)){
+                    leftest[1] = h;
+                    leftest[0] = w;
+                }
+                if(w > rightest.at(0)){
+                    rightest[1] = h;
+                    rightest[0] = w;
+                }
+            }
+        }
+    }
+    ss << "Lowest: x = " << lowest.at(0) << " y = " << lowest.at(1) << std::endl;
+    ss << "Highest: x = " << highest.at(0) << " y = " << highest.at(1) << std::endl;
+    ss << "Rightest: x = " << leftest.at(0) << " y = " << rightest.at(1) << std::endl;
+    ss << "Leftest: x = " << rightest.at(0) << " y = " << leftest.at(1) << std::endl;
+    std::cout << ss.str();
+    return (unsigned int)((lowest.at(1) - highest.at(1)) / 2);
+}
 
 void test_load(){
 
@@ -110,9 +161,15 @@ int main() {
   test_load();
   test_save();
   test_image();
-  vector<unsigned int> max = get_max_pixel("mate.0.ppm");
-    std::cout << "Position of maximum brightness: " << std::endl;
-    std::cout << max.at(0) << std::endl;
-    std::cout << max.at(1) << std::endl;
+    std::cout << "Choose a image from the folder: ...  ";
+    std::string filename = "";
+    std::cin >> filename;
+  vector<unsigned int> max = get_max_pixel(filename);
+    std::stringstream ss;
+    ss << "Position of maximum brightness: x = " << max.at(0) << " y = " << max.at(1) << std::endl;
+    std::cout << ss.str();
+    unsigned int radio = get_radio("mate.mask.ppm");
+    std::cout << radio << std::endl;
+    std::cout << filename << std::endl;
   return 0;
 }
