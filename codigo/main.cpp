199d204ced9fe4ce96a886c96ed231230c8fce09
Angel Yan <angelyan96@gmail.com>
2017-08-26 14:15:46 -0300

    Testeo de mascara y puntos mas brillantes

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -9,16 +9,26 @@ int main() {
     S.trasponer();
     //S.print();
     
+    PPM mascara = PPM("mate/mate.mask.ppm");
+    auto mask = mascara.generarMascara();
+    for (auto it = mask.begin(); it != mask.end(); ++it) {
+        mascara((*it).x, (*it).y, 0) = 255;
+        mascara((*it).x, (*it).y, 1) = 0;
+        mascara((*it).x, (*it).y, 2) = 0;
+    }
+    mascara.guardarImagen("mate2/mate.mask.ppm");
     PPM imgs[12];
     for (int i = 0; i < 12; ++i) {
         stringstream f;
         f << "mate/mate." << i << ".ppm";
         imgs[i].cargarImagen(f.str());
+        imgs[i].aplicarMascara(&mask);
+        //imgs[i].eliminarMascara();
         auto v = imgs[i].puntosMasBrillantes();
         for (uint j = 0; j < v.size(); ++j) {
-            imgs[i](v.at(j).first, v.at(j).second, 0) = 255;
-            imgs[i](v.at(j).first, v.at(j).second, 1) = 0;
-            imgs[i](v.at(j).first, v.at(j).second, 2) = 0;
+            imgs[i](v.at(j).x, v.at(j).y, 0) = 255;
+            imgs[i](v.at(j).x, v.at(j).y, 1) = 0;
+            imgs[i](v.at(j).x, v.at(j).y, 2) = 0;
         }
         stringstream o;
         o << "mate2/mate." << i << ".ppm";
