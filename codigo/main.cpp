d7a2de255fee38941922bf51c254ad3a7940590e
Santiago Feliu <sfeliu@dc.uba.ar>
2017-09-01 14:49:45 -0300

    incluyo el codigo para conseguir todas las posibles matrices para buscar el mejor numero de condicion.

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -41,6 +41,63 @@ PPM::punto puntoDeMayorIntensidad(PPM &ppm, pair<PPM::punto, PPM::punto> masc) {
     }
 }
 
+void conseguir_Todas_Posibilidades(){
+    std::vector< Matriz > filas;
+    double fila0[3] = {0.403259,0.480808,0.778592};
+    Matriz f0(1,3,fila0,3);
+    filas.push_back(f0);
+    double fila1[3] = {0.0982272,0.163712,0.981606};
+    Matriz f1(1,3,fila1,3);
+    filas.push_back(f1);
+    double fila2[3] = {-0.0654826,0.180077,0.98147};
+    Matriz f2(1,3,fila2,3);
+    filas.push_back(f2);
+    double fila3[3] = {-0.127999,0.431998,0.892745};
+    Matriz f3(1,3,fila3,3);
+    filas.push_back(f3);
+    double fila4[3] = {-0.328606,0.485085,0.810377};
+    Matriz f4(1,3,fila4,3);
+    filas.push_back(f4);
+    double fila5[3] = {-0.110339,0.53593,0.837021};
+    Matriz f5(1,3,fila5,3);
+    filas.push_back(f5);
+    double fila6[3] = {0.239071,0.41439,0.878138};
+    Matriz f6(1,3,fila6,3);
+    filas.push_back(f6);
+    double fila7[3] = {0.0642302,0.417497,0.906406};
+    Matriz f7(1,3,fila7,3);
+    filas.push_back(f7);
+    double fila8[3] = {0.12931,0.339438,0.931698};
+    Matriz f8(1,3,fila8,3);
+    filas.push_back(f8);
+    double fila9[3] = {0.0323953,0.340151,0.939813};
+    Matriz f9(1,3,fila9,3);
+    filas.push_back(f9);
+    double fila10[3] = {0.0985318,0.0492659,0.993914};
+    Matriz f10(1,3,fila10,3);
+    filas.push_back(f10);
+    double fila11[3] = {-0.16119,0.354617,0.921013};
+    Matriz f11(1,3,fila11,3);
+    filas.push_back(f11);
+    ofstream todas;
+    vector<double> numeros_condicion;
+    todas.open("todas_las_matrices.txt");
+    for (uint i=0; i<10; ++i){
+        for (uint j=i+1; j<11; ++j){
+            for (uint k=j+1; k<12; ++k){
+                //  todas << "F1 " << filas.at(i)(0,0) << " , " << filas.at(i)(0,1) << " , " << filas.at(i)(0,2) << "\n";
+                //todas << "F2 " << filas.at(j)(0,0) << " , " << filas.at(i)(0,1) << " , " << filas.at(i)(0,2) << "\n";
+                //  todas << "F3 " << filas.at(k)(0,0) << " , " << filas.at(i)(0,1) << " , " << filas.at(i)(0,2) << "\n\n";
+                todas << filas.at(i)(0,0) << ',' << filas.at(i)(0,1) << ',' << filas.at(i)(0,2) << ',';
+                todas << filas.at(j)(0,0) << ',' << filas.at(i)(0,1) << ',' << filas.at(i)(0,2) << ',';
+                todas << filas.at(k)(0,0) << ',' << filas.at(i)(0,1) << ',' << filas.at(i)(0,2) << "\n";
+
+            }
+        }
+    }
+    todas.close();/*
+}
+
 Matriz matrizDeIntensidades(vector<PPM> &ppms, const int x, const int y) {
     Matriz m = Matriz(ppms.size(), 1);
     for (int i = 0; i < (int)ppms.size(); ++i) {
