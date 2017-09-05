aaf105fea21c6dd81a88a74a54313e236e3cb879
Angel Yan <angelyan96@gmail.com>
2017-09-05 01:28:21 -0300

    Implemento eleccion de direcciones por numero de condicion y otros

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -64,63 +64,6 @@ PPM::punto puntoDeMayorIntensidad(const PPM &ppm, pair<PPM::punto, PPM::punto> m
     }
 }
 
-void conseguir_Todas_Posibilidades(){
-    std::vector< Matriz > filas;
-    double fila0[3] = {0.403259,0.480808,0.778592};
-    Matriz f0(1,3,fila0,3);
-    filas.push_back(f0);
-    double fila1[3] = {0.0982272,0.163712,0.981606};
-    Matriz f1(1,3,fila1,3);
-    filas.push_back(f1);
-    double fila2[3] = {-0.0654826,0.180077,0.98147};
-    Matriz f2(1,3,fila2,3);
-    filas.push_back(f2);
-    double fila3[3] = {-0.127999,0.431998,0.892745};
-    Matriz f3(1,3,fila3,3);
-    filas.push_back(f3);
-    double fila4[3] = {-0.328606,0.485085,0.810377};
-    Matriz f4(1,3,fila4,3);
-    filas.push_back(f4);
-    double fila5[3] = {-0.110339,0.53593,0.837021};
-    Matriz f5(1,3,fila5,3);
-    filas.push_back(f5);
-    double fila6[3] = {0.239071,0.41439,0.878138};
-    Matriz f6(1,3,fila6,3);
-    filas.push_back(f6);
-    double fila7[3] = {0.0642302,0.417497,0.906406};
-    Matriz f7(1,3,fila7,3);
-    filas.push_back(f7);
-    double fila8[3] = {0.12931,0.339438,0.931698};
-    Matriz f8(1,3,fila8,3);
-    filas.push_back(f8);
-    double fila9[3] = {0.0323953,0.340151,0.939813};
-    Matriz f9(1,3,fila9,3);
-    filas.push_back(f9);
-    double fila10[3] = {0.0985318,0.0492659,0.993914};
-    Matriz f10(1,3,fila10,3);
-    filas.push_back(f10);
-    double fila11[3] = {-0.16119,0.354617,0.921013};
-    Matriz f11(1,3,fila11,3);
-    filas.push_back(f11);
-    ofstream todas;
-    vector<double> numeros_condicion;
-    todas.open("todas_las_matrices.txt");
-    for (uint i=0; i<10; ++i){
-        for (uint j=i+1; j<11; ++j){
-            for (uint k=j+1; k<12; ++k){
-                //  todas << "F1 " << filas.at(i)(0,0) << " , " << filas.at(i)(0,1) << " , " << filas.at(i)(0,2) << "\n";
-                //todas << "F2 " << filas.at(j)(0,0) << " , " << filas.at(i)(0,1) << " , " << filas.at(i)(0,2) << "\n";
-                //  todas << "F3 " << filas.at(k)(0,0) << " , " << filas.at(i)(0,1) << " , " << filas.at(i)(0,2) << "\n\n";
-                todas << filas.at(i)(0,0) << ',' << filas.at(i)(0,1) << ',' << filas.at(i)(0,2) << ',';
-                todas << filas.at(j)(0,0) << ',' << filas.at(i)(0,1) << ',' << filas.at(i)(0,2) << ',';
-                todas << filas.at(k)(0,0) << ',' << filas.at(i)(0,1) << ',' << filas.at(i)(0,2) << "\n";
-
-            }
-        }
-    }
-    todas.close();
-}
-
 Matriz matrizDeIntensidades(const vector<PPM> &ppms, const int x, const int y) {
     Matriz m = Matriz(ppms.size(), 1);
     for (int i = 0; i < (int)ppms.size(); ++i) {
@@ -141,20 +84,26 @@ int main() {
     
     // 1. Calibracion del sistema
     
+    int dirs_cant;
+    int elecDirsI[3];
+    Matriz S(3);
+    
     ifstream calibracion_in("calibracion.txt");
-    vector<Matriz> dirsI;
     if (calibracion_in.is_open()) {
-        int dirs_cant;
+        // Leo la cantidad total de direcciones
         calibracion_in >> dirs_cant;
-        for (int k = 0; k < dirs_cant; ++k) {
-            dirsI.push_back(Matriz(3, 1));
-            for (int i = 0; i < 3; ++i)
-                calibracion_in >> dirsI[k](i,0);
+        // Leo la eleccion de direcciones realizada
+        for (int i = 0; i < 3; ++i) calibracion_in >> elecDirsI[i];
+        // Cargo las direcciones elegidas en S
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) calibracion_in >> S(i,j);
         }
         calibracion_in.close();
     } else {
+        
 
     // 1.1. Lectura de imagenes mate
+
     cout << "Calibracion del sistema" << endl;
     cout << "Ingrese el archivo de texto de rutas del modelo mate: ";
     string mate_src_path;
@@ -162,54 +111,96 @@ int main() {
     mate_src_path = "mate.txt"; cout << endl;
     ifstream mate_src(mate_src_path);
     if (!mate_src.is_open()) throw runtime_error("ERROR: no se pudo abrir el archivo");
-    int mate_cant;
-    mate_src >> mate_cant; // leo la cantidad de imagenes que no son mascara
+    
+    mate_src >> dirs_cant; // leo la cantidad de imagenes (excluyendo mascara)
     mate_src.ignore(numeric_limits<std::streamsize>::max(), '\n'); // voy hasta la proxima linea
-    vector<PPM> mate(mate_cant);
+    vector<PPM> mate(dirs_cant);
     PPM mate_mask;
     {
         string ruta;
-        for (int i = 0; i < mate_cant; ++i) {
+        for (int i = 0; i < dirs_cant; ++i) {
             getline(mate_src, ruta);
             mate[i].cargarImagen(ruta);
         }
         mate_mask.cargarImagen(ruta);
     }
     
+    
     // 1.2. Obtenencion de direcciones de iluminacion
+    
     pair<PPM::punto, PPM::punto> mate_mask_pts = mate_mask.generarMascara(); // obtengo puntos de la mascara
     int radio = (mate_mask_pts.second.x - mate_mask_pts.first.x + 1) / 2; // radio de la esfera
     PPM::punto centro(mate_mask_pts.first.x + radio - 1, mate_mask_pts.first.y + radio - 1); // centro de la esfera
-    ofstream calibracion_out;
-    calibracion_out.open("calibracion.txt");
-    calibracion_out << mate_cant << '\n';
-    for (int i = 0; i < mate_cant; ++i) {
+    
+    // Obtengo direcciones de iluminacion
+    vector<Matriz> dirsI(dirs_cant, Matriz(3,1));
+    for (int i = 0; i < dirs_cant; ++i) {
         PPM::punto pt = puntoDeMayorIntensidad(mate[i], mate_mask_pts);
-        dirsI.push_back(Matriz(3,1));
         dirsI[i](0,0) = pt.x - centro.x; // coordenada x
         dirsI[i](1,0) = pt.y - centro.y; // coordenada y
         dirsI[i](2,0) = pow(pow(radio, 2) - pow(dirsI[i](0,0), 2) - pow(dirsI[i](1,0), 2), 0.5); // coordenada z
         dirsI[i] = dirsI[i] * (1 / (double)radio); // normalizo el vector
-        calibracion_out << dirsI[i](0,0) << ' ';
-        calibracion_out << dirsI[i](1,0) << ' ';
-        calibracion_out << dirsI[i](2,0) << '\n';
     }
-    calibracion_out.close();
-
-    }
-    // 2. Reconstruccion del modelo 3D
     
-    // 2.1. Eleccion de direcciones de iluminacion
-    int elecDirsI[4] = {0, 4, 10}; // menor numero de condición
-    //int elecDirsI[4] = {3, 4, 8}; // mayor numero de condición
-    Matriz S(3); // matriz de direcciones de iluminacion
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            S(i,j) = dirsI[elecDirsI[i]](j,0);
+    
+    // 1.3. Eleccion de direcciones de iluminacion
+    
+    // Elijo las direcciones que formen la matriz con menor numero de condicion para minimizar errores de estimacion
+    double min_num_cond = INFINITY;
+    for (int i = 0; i < dirs_cant-2; ++i) {
+        for (int j = i+1; j < dirs_cant-1; ++j) {
+            for (int k = j+1; k < dirs_cant; ++k) {
+                Matriz A(3);
+                A(0,0) = dirsI[i](0,0); A(0,1) = dirsI[i](1,0); A(0,2) = dirsI[i](2,0);
+                A(1,0) = dirsI[j](0,0); A(1,1) = dirsI[j](1,0); A(1,2) = dirsI[j](2,0);
+                A(2,0) = dirsI[k](0,0); A(2,1) = dirsI[k](1,0); A(2,2) = dirsI[k](2,0);
+                double norma_A = A.normaF(); // guardo la norma de S
+                // Aplico eliminacion Gaussiana ampliando con la matriz identidad para obtener la inversa de S
+                Matriz I = Matriz::Identidad(3);
+                A.eliminacionGaussiana(I);
+                // Si existe la inversa calculo el numero de condicion
+                if (!eq(A(2,2), 0)) {
+                    Matriz B;
+                    A.backwardSubstitution(B, I); // obtengo la inversa
+                    double num_cond = norma_A * B.normaF();
+                    // Actualizo el minimo numero de condicion y guardo los indices de las direcciones
+                    if (lt(num_cond, min_num_cond)) {
+                        min_num_cond = num_cond;
+                        elecDirsI[0] = i;
+                        elecDirsI[1] = j;
+                        elecDirsI[2] = k;
+                    }
+                }
+            }
         }
     }
     
-    // 2.2. Lectura de imagenes del modelo a reconstruir
+    // Defino matriz S
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) S(i,j) = dirsI[elecDirsI[i]](j,0);
+    }
+    
+    
+    // 1.4. Exportacion de datos de calibracion
+    ofstream calibracion_out;
+    calibracion_out.open("calibracion.txt");
+    calibracion_out << dirs_cant << '\n';
+    calibracion_out << elecDirsI[0] << ' ';
+    calibracion_out << elecDirsI[1] << ' ';
+    calibracion_out << elecDirsI[2] << '\n';
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) calibracion_out << S(i,j) << ' ';
+    }
+    calibracion_out.close();
+    
+
+    }
+    
+    
+    // 2. Reconstruccion del modelo 3D
+    
+    
+    // 2.1. Lectura de imagenes del modelo a reconstruir
     cout << "Reconstruccion del modelo 3D" << endl;
     cout << "Ingrese el archivo de texto de rutas del modelo a reconstruir: ";
     string modelo_src_path;
@@ -221,7 +212,8 @@ int main() {
     PPM modelo_mask;
     {
         int modelo_cant;
-        modelo_src >> modelo_cant; // leo la cantidad de imagenes que no son mascara
+        modelo_src >> modelo_cant; // leo la cantidad de imagenes (excluyendo mascara)
+        if (modelo_cant != dirs_cant) throw runtime_error("ERROR: la cantidad de imagenes del modelo no coincide con la cantidad de direcciones de iluminacion provistas en la calibracion");
         modelo_src.ignore(numeric_limits<std::streamsize>::max(), '\n'); // voy hasta la proxima linea
         int j = 0;
         string ruta;
@@ -236,7 +228,8 @@ int main() {
         modelo_mask.cargarImagen(ruta); // cargar mascara
     }
     
-    // 2.3. Construccion del campo normal y estimacion de profundidades
+    
+    // 2.2. Construccion del campo normal y estimacion de profundidades
     
     pair<PPM::punto, PPM::punto> modelo_mask_pts = modelo_mask.generarMascara(); // obtengo puntos de la mascara
     
@@ -261,20 +254,18 @@ int main() {
     // Obtengo la factorizacion PLU de S
     Matriz P, L, U;
     S.factorizacionPLU(P, L, U); // PS = LU
-    //S.invertir();
     for (int y = modelo_mask_pts.first.y; y <= modelo_mask_pts.second.y; ++y) {
         for (int x = modelo_mask_pts.first.x; x <= modelo_mask_pts.second.x; ++x) {
             // Resolucion de ecuacion Sn = b <=> PSn = Pb <=> LUn = Pb
             Matriz Pb = P * matrizDeIntensidades(modelo, x, y);
             Matriz m; L.forwardSubstitution(m, Pb); // resuelvo ecuacion Lm = Pb (m = Un)
             Matriz n; U.backwardSubstitution(n, m); // resuelvo ecuacion Un = m
-            //Matriz n = S * matrizDeIntensidades(modelo, x, y);
             double norma2 = n.normaF();
             if (!eq(norma2, 0))
                 n = (1/norma2) * n; // normalizo el vector para obtener la normal
             // Escribo coordenadas en archivos de texto
             normales_x << n(0,0) << ',';
-            normales_y << -n(1,0) << ',';
+            normales_y << n(1,0) << ',';
             normales_z << n(2,0) << ',';
             if (x == modelo_mask_pts.second.x - 1) {
                 normales_x << endl;
@@ -309,7 +300,7 @@ int main() {
     // Resolucion de ecuacion Az = b <=> CC_tz = b
     Matriz u; C.forwardSubstitution(u, b); // resuelvo ecuacion Cu = b (u = C_tz)
     Matriz z; C.trasponer().backwardSubstitution(z, b); // resuelvo ecuacion C_tz = u
-    
+
 
     return 0;   
 }
