93f31376905bb97fbbc306699e0e1e9f86688933
Santiago Feliu <sfeliu@dc.uba.ar>
2017-09-06 16:29:04 -0300

    Agrego la matriz gaussiana

--- codigo/experimentacion.cpp
+++ codigo/experimentacion.cpp
@@ -36,6 +36,13 @@ double random02(){
     return randomNumber;
 }
 
+double random03(int n){
+
+    unsigned int time_ui = static_cast<unsigned int>( time(NULL) );
+    srand( time_ui + n);
+    return ((double) rand() / (RAND_MAX));
+}
+
 Matriz randomMatriz(int f, int c){
     int tamano = f*c;
     double matrizRandom[tamano];
@@ -62,14 +69,42 @@ Matriz randomMatrizSDP(int n){
     return r1;
 }
 
+Matriz eliminacionGaussiana(Matriz a,Matriz b) {
+    int f = 0, c = 0;
+    Matriz nueva = a;
+    while (f < nueva.filas() && c < nueva.columnas()) {
+        // Busco fila de pivoteo parcial
+        int i = f; // fila de pivoteo parcial
+        for (int k = i + 1; k < nueva.filas(); ++k) {
+            if (fabs(nueva(i,c)) < fabs(nueva(k,c)))
+                i = k;
+        }
+        if (!eq(nueva(i,c), 0)) { // si hay fila de pivoteo parcial
+            // Hago el pivoteo (se rompe el invariante de banda p, q)
+            b.permutarFila(f, i);
+            nueva.permutarFila(f, i);
+            for (int k = f + 1; k < nueva.filas(); ++k) {
+                if (!eq(nueva(k,c), 0)) {
+                    double m = nueva(k,c) / nueva(f,c);
+                    b.restarMultiploDeFila(k, f, m);
+                    nueva.restarMultiploDeFila(k, f, m);
+                }
+            }
+            ++f;
+        }
+        ++c;
+    }
+    return nueva;
+}
+
 void testEliminacionGaussiana(vector<Matriz> terminosIndependientes, int filas){
     cout<<"Empezando prueba de eliminación gaussiana con " << terminosIndependientes.size() << " terminos independientes" << endl;
     int tamano = terminosIndependientes.size();
     Matriz random = randomMatrizSDP(filas);
     clock_t start = clock();
     for(int i=0;i<tamano;i++){
-        random.eliminacionGaussiana(terminosIndependientes[i]);
-        Matriz x; random.backwardSubstitution(x,terminosIndependientes[i]);
+        Matriz A = eliminacionGaussiana(random, terminosIndependientes[i]);
+        Matriz x; A.backwardSubstitution(x,terminosIndependientes[i]);
     }
     clock_t end = clock();
     double segs = (double) (end - start) / CLOCKS_PER_SEC;
@@ -123,7 +158,7 @@ int main(){
     for(int i=0; i<tamano; i++){
         Matriz temporal = Matriz(filas,1);
         for(int j=0; j<filas;j++){
-            temporal(j,0) = random02();
+            temporal(j,0) = random03(j);
         }
         terminosIndependientes.push_back(temporal);
     }
